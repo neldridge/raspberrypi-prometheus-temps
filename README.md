061d7bccc5e87a17d4f53eed9c13eb92072006cf
Nick Eldridge <nicholas.eldridge@gmail.com>
2024-01-15 20:07:52 -0500

    Adding support for Jetsons Changing how thermal_zones are handled Updating vcgencmd to be a "custom" command, we can now scale any custom commands for any platform

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Temperature Exporter
 The intent of this project is to provide GPU/CPU and any other relevant temperatures for devices to be exported on `/metrics` for consumption within Prometheus
+
+# Raspberry PI OS
+Tested with Raspberry PI 4/5 and CM4, GPU attained from `vcgencmd measure_temp` and any other temp (cpu) from `/sys/class/thermal/thermal_zone?/{temp,type}`
+
+## Jetson TX Temperatures
+Essentially, these should be covered by iterating every `/sys/class/thermal/thermal_zone?/{temp,type}`
+
+Relevant info:
+* https://docs.nvidia.com/jetson/archives/r34.1/DeveloperGuide/text/SD/PlatformPowerAndPerformance/JetsonOrinNxSeriesAndJetsonAgxOrinSeries.html#bsp-specific-thermal-zones
+* https://forums.developer.nvidia.com/t/use-c-language-get-cpu-thermal-sensor-temperature-tx1/52275/3
+* https://forums.developer.nvidia.com/t/thermal-zone4-reports-100-degree-celcius/44422/6
--- cmd/temp-export/main.go
+++ cmd/temp-export/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log/syslog"
 	"net/http"
+	"os"
 	"os/exec"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -12,11 +15,15 @@ import (
 	lSyslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
-var log logrus.Logger
-var config Config
 var (
-	VERSION   string = "undefined"
-	BUILDTIME string = "manual"
+	VERSION               string = "undefined"
+	BUILDTIME             string = "manual"
+	config                Config
+	log                   logrus.Logger
+	board                 string           = "unknown"
+	customCommandsToCheck []CustomCommands = []CustomCommands{
+		{ThermalType: "gpu", Command: "vcgencmd", Args: []string{"measure_temp"}, Regex: "temp=([0-9.]+)'C"},
+	}
 )
 
 type Config struct {
@@ -25,19 +32,105 @@ type Config struct {
 	LogFilename    string
 }
 
-func getGPUTemperature() (float64, error) {
-	cmd := exec.Command("vcgencmd", "measure_temp")
-	output, err := cmd.Output()
-	// Check if cmd not found and set to 0
-	if err != nil {
-		return 0, err
+type CustomCommands struct {
+	Command     string
+	Args        []string
+	Regex       string
+	ThermalType string
+}
+
+type Temperature struct {
+	Device string
+	Temp   float64
+}
+
+func checkExists(path string) bool {
+	// log.Debugf("Checking if %s exists", path)
+	// path should be split on space then be [0]
+	path = strings.Split(path, " ")[0]
+	path = strings.TrimSpace(path)
+	// log.Debugf("Checking if %s exists (now split/trimmed)", path)
+
+	// File may not be executable, we should check if the file exists at all
+	_, err := os.Stat(path)
+
+	if err == nil {
+		log.Debugf("Found %s", path)
+		return true
 	}
-	if strings.Contains(string(output), "not found") {
-		return 0, nil
+
+	// Check if "which <path>" in case of command
+	// log.Debugf("Checking if %s exists (now checking which)", path)
+	whichPath, err := exec.Command("which", path).Output()
+	if err == nil {
+		// log.Debugf("Found %s", string(whichPath))
+		return checkExists(string(whichPath))
 	}
-	temperature := strings.TrimPrefix(string(output), "temp=")
-	temperature = strings.TrimSpace(strings.TrimSuffix(temperature, "'C\n"))
-	// convert to int32
+	// log.Debugf("Failed to find %s", path)
+	return false
+
+}
+
+func determineBoard() string {
+
+	log.Debugf("Determining board type...")
+	// Check if we're on a tegra
+	if checkExists("/usr/bin/jetson_release") {
+		/*
+			# jetson_release
+			Software part of jetson-stats 4.2.3 - (c) 2023, Raffaello Bonghi
+			Model: lanai-3636 - Jetpack 4.6.4 [L4T 32.7.4]
+			NV Power Mode[3]: MAXP_CORE_ARM
+			Serial Number: [XXX Show with: jetson_release -s XXX]
+			Hardware:
+			 - P-Number: p3636-0001
+			 - Module: NVIDIA Jetson TX2 NX
+			Platform:
+			 - Distribution: Ubuntu 18.04 Bionic Beaver
+			 - Release: 4.9.337-tegra
+			jtop:
+			 - Version: 4.2.3
+			 - Service: Active
+			Libraries:
+			 - CUDA: 10.2.300
+			 - cuDNN: 8.2.1.32
+			 - TensorRT: 8.2
+			 - VPI: 1.2.3
+			 - Vulkan: 1.2.70
+			 - OpenCV: 4.1.1 - with CUDA: NO
+		*/
+		log.Debugf("Found /usr/bin/jetson_release")
+		return "tegra"
+	} else if checkExists("/sys/firmware/devicetree/base/model") {
+		// check if "Raspberry PI" is in the model
+
+		// open the file with ioutil
+		// check if it contains "Raspberry Pi"
+		log.Debugf("Found /sys/firmware/devicetree/base/model")
+		model, err := os.ReadFile("/sys/firmware/devicetree/base/model")
+		if err == nil {
+			if strings.Contains(string(model), "Raspberry Pi") {
+				log.Debugf("Found Raspberry Pi in model: %s", string(model))
+				return "raspberrypi"
+			} else {
+				log.Debugf("Did not find Raspberry Pi in model")
+				log.Debugf("Model: %s", string(model))
+			}
+		}
+
+	}
+	log.Debugf("Failed to determine board type")
+	return "unknown"
+
+}
+func extractTemperature(input string, regex string) (float64, error) {
+	re := regexp.MustCompile(regex)
+	match := re.FindStringSubmatch(input)
+	if match == nil || len(match) < 2 {
+		return 0, fmt.Errorf("Temperature not found in the input string")
+	}
+	// must be float
+	temperature := strings.TrimSpace(match[1])
 	temperatureInt, err := strconv.ParseFloat(temperature, 64)
 	if err != nil {
 		return 0, err
@@ -45,36 +138,117 @@ func getGPUTemperature() (float64, error) {
 	return temperatureInt, nil
 }
 
-func getCPUTemperature() (float64, error) {
-	content, err := exec.Command("cat", "/sys/class/thermal/thermal_zone0/temp").Output()
-	if err != nil {
-		return 0, err
+func getCustomCommands() ([]Temperature, []error) {
+	tempsToReturn := []Temperature{}
+	errors := []error{}
+
+	for _, command := range customCommandsToCheck {
+		if checkExists(command.Command) {
+			cmd := exec.Command(command.Command, command.Args...)
+
+			output, err := cmd.Output()
+			// Check if cmd not found and set to 0
+			if err != nil {
+				errors = append(errors, err)
+				tempsToReturn = append(tempsToReturn, Temperature{Device: command.ThermalType, Temp: 0})
+				continue
+			}
+			temperature, err := extractTemperature(string(output), command.Regex)
+			if err != nil {
+				errors = append(errors, err)
+				tempsToReturn = append(tempsToReturn, Temperature{Device: command.ThermalType, Temp: 0})
+				continue
+			}
+			// Lowercase the thermaltype
+			command.ThermalType = strings.ToLower(command.ThermalType)
+			tempsToReturn = append(tempsToReturn, Temperature{Device: command.ThermalType, Temp: temperature})
+		}
 	}
-	temperature := strings.TrimSpace(string(content))
-	temperature = temperature[:len(temperature)-3] + "." + temperature[len(temperature)-3:]
-	temperatureInt, err := strconv.ParseFloat(temperature, 64)
+	return tempsToReturn, errors
+}
+
+func getThermalZones() ([]Temperature, []error) {
+	// We need to get `temp` and `type` from each thermal zone
+	// We will iterate every /sys/class/thermal/thermal_zone#/ directory
+
+	tempsToReturn := []Temperature{}
+	errors := []error{}
+
+	zones, err := os.ReadDir("/sys/class/thermal/")
 	if err != nil {
-		return 0, err
+		errors = append(errors, err)
+		return tempsToReturn, errors
 	}
-	return temperatureInt, nil
+
+	for _, zone := range zones {
+		log.Debugf("Maybe found thermal zone: %s", zone.Name())
+
+		// Check if the zone is a thermal zone
+		if !strings.HasPrefix(zone.Name(), "thermal_zone") {
+			log.Debugf("! HasPrefix(thermal_zone): %s", zone.Name())
+			continue
+		}
+
+		// Check if the zone has a type
+		typeFile, err := os.ReadFile(fmt.Sprintf("/sys/class/thermal/%s/type", zone.Name()))
+		if err != nil {
+			errors = append(errors, err)
+			continue
+		}
+
+		deviceType := strings.TrimSpace(string(typeFile))
+		// Lowercase the device type
+		deviceType = strings.ToLower(deviceType)
+		// Remove "-thermal" and "-therm" from the end of the string
+		deviceType = strings.TrimSuffix(deviceType, "-thermal")
+		deviceType = strings.TrimSuffix(deviceType, "-therm")
+
+		// Check if the zone has a temp
+		tempFile, err := ioutil.ReadFile(fmt.Sprintf("/sys/class/thermal/%s/temp", zone.Name()))
+		if err != nil {
+			errors = append(errors, err)
+			continue
+		}
+		temperature := strings.TrimSpace(string(tempFile))
+		temperature = temperature[:len(temperature)-3] + "." + temperature[len(temperature)-3:]
+		temperatureInt, err := strconv.ParseFloat(temperature, 64)
+		if err != nil {
+			errors = append(errors, err)
+			continue
+		}
+		tempsToReturn = append(tempsToReturn, Temperature{Device: deviceType, Temp: temperatureInt})
+
+	}
+
+	return tempsToReturn, errors
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	gpuTemp, err := getGPUTemperature()
-	if err != nil {
-		// http.Error(w, fmt.Sprintf("Failed to get GPU temperature: %s", err), http.StatusInternalServerError)
-		log.Errorf("Failed to get GPU temperature: %s", err)
-		gpuTemp = 0
+
+	tempsTmp, err := getCustomCommands()
+	// check if err is an empty list
+	if len(err) > 0 {
+		for _, e := range err {
+			log.Errorf("Failed to get custom commands: %s", e)
+		}
+	}
+	for _, temp := range tempsTmp {
+		log.Debugf("Found temperature: %s: %f", temp.Device, temp.Temp)
+		fmt.Fprintf(w, "%s_temperature{device=\"%s\"} %f\n", temp.Device, temp.Device, temp.Temp)
 	}
 
-	cpuTemp, err := getCPUTemperature()
-	if err != nil {
-		// http.Error(w, fmt.Sprintf("Failed to get CPU temperature: %s", err), http.StatusInternalServerError)
-		log.Errorf("Failed to get CPU temperature: %s", err)
-		cpuTemp = 0
+	tempsTmp, err = getThermalZones()
+	if len(err) > 0 {
+		for _, e := range err {
+			log.Errorf("Failed to get custom commands: %s", e)
+		}
+	}
+	for _, temp := range tempsTmp {
+		log.Debugf("Found temperature: %s: %f", temp.Device, temp.Temp)
+		fmt.Fprintf(w, "%s_temperature{device=\"%s\"} %f\n", temp.Device, temp.Device, temp.Temp)
 	}
 
-	fmt.Fprintf(w, "gpu_temperature{device=\"gpu\"} %f\ncpu_temperature{device=\"cpu\"} %f\nversion{app=\"%s\", build_time=\"%s\"}", gpuTemp, cpuTemp, VERSION, BUILDTIME)
+	fmt.Fprintf(w, "version{app=\"%s\", build_time=\"%s\"}", VERSION, BUILDTIME)
 }
 
 func main() {
@@ -123,7 +297,21 @@ func main() {
 
 	log.Infof("Temperature Exporter github.com/neldridge/temperature-exporter v%s (built: %s)", VERSION, BUILDTIME)
 
+	board = determineBoard()
+
+	if board == "raspberrypi" {
+		log.Infof("Detected Raspberry Pi")
+	} else if board == "tegra" {
+		log.Infof("Detected Tegra based board")
+	} else {
+		log.Warnf("Unknown board")
+	}
+
 	http.HandleFunc("/metrics", handler)
 	log.Infof("Server listening on :8080")
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Failed to start server: %s", err)
+	}
+
 }
